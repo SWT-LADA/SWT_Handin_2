7dccfc2634515a0f19ff08141e982c06759d3eff
Dikte Straadt <Dikte.straadt@hotmail.com>
2020-03-11 15:20:40 +0100

    Code added to main

--- LadeskabConsoleApp/Program.cs
+++ LadeskabConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ladeskab.DoorEvent;
+using Ladeskab.RFIDEvent;
 
 namespace LadeskabConsoleApp
 {
@@ -10,6 +12,44 @@ namespace LadeskabConsoleApp
     {
         static void Main(string[] args)
         {
+            Door door = new Door();
+            RFIDReader rfidReader = new RFIDReader();
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.SetDoorState();
+                        break;
+
+                    case 'C':
+                        door.SetDoorState();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.SetRFID(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
         }
     }
 }
