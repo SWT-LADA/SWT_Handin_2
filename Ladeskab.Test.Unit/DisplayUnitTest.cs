54fae79a15cac7febd84e474391a5a5bfcc91f6d
Dikte Straadt <Dikte.straadt@hotmail.com>
2020-03-24 10:05:56 +0100

    DisplayUnitTest class added

---
+++ Ladeskab.Test.Unit/DisplayUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Ladeskab;
+using Ladeskab.DisplayFolder;
+//using NSubstitute;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class DisplayUnitTest
+    {
+        private IDisplay _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+        }
+
+        [Test]
+        public void Test()
+        {
+            MemoryStream stream = new MemoryStream();
+            TextWriter writer = new StreamWriter(stream);
+            TextReader reader = new StreamReader(stream);
+
+        }
+    }
+}
